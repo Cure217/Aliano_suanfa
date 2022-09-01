2aab65966f14132b74a40dba58c328124251d082
Cure <1318822416@qq.com>
2022-09-01 20:10:14 +0800

    冒泡排序的实现

---
+++ src/com/aliano/SF_day01/Code01_SelectionSort.java
@@ -0,0 +1,38 @@
+package com.aliano.SF_day01;
+
+/**
+ * Created by Intellij IDEA.
+ *
+ * @author Cure
+ * @create 2022/9/1 19:56
+ *
+ * 选择排序的实现
+ */
+public class Code01_SelectionSort {
+    public static void main(String[] args) {
+        int [] array = {2,5,63,7,6,89};
+        selectionSort(array);
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+
+    public static void selectionSort(int [] arr){
+        if (arr ==null || arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+             int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+            }
+            swap(arr,i,minIndex);
+        }
+    }
+
+    public static void swap(int [] arr, int i, int j){
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
